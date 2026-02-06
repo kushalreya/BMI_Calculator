a2528fd21d4bc3433c395fab793da037a4610b4c
Kush <kushalreya@gmail.com>
2026-02-06 21:39:41 +0530

    comment codes are removed

--- app/src/main/java/sc/android/bmi_calculator/ui/theme/Color.kt
+++ app/src/main/java/sc/android/bmi_calculator/ui/theme/Color.kt
@@ -1,29 +1,3 @@
-
-/*
-package sc.android.bmi_calculator.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-// Backgrounds
-val DarkBackground = Color(0xFF1F2023)
-val CardBackground = Color(0xFF25262A)       // TextField fill
-val BorderColor = Color(0xFF4D4F54)           // TextField border
-
-// Text
-val PrimaryText = Color(0xFFF2F2F2)           // Title, main text
-val SecondaryText = Color(0xFFB8B9BD)         // Labels (Height, Weight)
-val HintText = Color(0xFF7E8086)              // "Enter Height"
-
-// Accent
-val AccentBlue = Color(0xFF5C6FA8)             // Button, switch track
-val AccentBlueDark = Color(0xFF4A5E95)
-
-// Button
-val ButtonTextColor = Color(0xFFFFFFFF)
-
-// Units (kg / m)
-val UnitTextColor = Color(0xFFE6E6E6)*/
-
 package sc.android.bmi_calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
